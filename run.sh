536f5de837a69c7eedd7d4580e1a30be05f6f497
mohamedhajja <mohamedhajja22@gmail.com>
2025-01-21 17:33:22 +0100

    Refactor script for robust PID handling and process management

--- run.sh
+++ run.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
-#Run API
-API_PID_FILE=api_pid.txt
-if test -f "$API_PID_FILE";
-then
-    pid=`cat "$API_PID_FILE"`
-    echo $pid
-    if [ "$pid" != '' ]
-    then
-        kill -9 $pid
-        rm "$API_PID_FILE"
-    fi
-    echo "RUNNING INFERENCE API"
-    nohup sh -c 'poetry run python data_ml_assignment/api/main.py' 2>&1 > api.log &
-    echo $! > "$API_PID_FILE"
-fi
 
-STREAMLIT_PID_FILE=streamlit_pid.txt
-if test -f "$STREAMLIT_PID_FILE";
-then
-    pid=`cat "$STREAMLIT_PID_FILE"`
-    echo $pid
-    if [ "$pid" != '' ]
-    then
-        kill -9 $pid
-        rm "$STREAMLIT_PID_FILE"
+# Function to handle PID files and processes
+handle_pid_file() {
+    local pid_file=$1
+    local start_command=$2
+    local log_file=$3
+    echo "Checking for existing PID in $pid_file"
+
+    if [ -f "$pid_file" ]; then
+        pid=$(cat "$pid_file")
+        echo "Found PID: $pid"
+        
+        if [ -n "$pid" ] && kill -0 "$pid" 2>/dev/null; then
+            echo "Stopping process $pid"
+            kill -9 "$pid"
+        else
+            echo "No running process found with PID $pid"
+        fi
+        rm "$pid_file"
     fi
-    echo "RUNNING STREAMLIT DASHBOARD"
-    nohup sh -c 'poetry run streamlit run --server.port 8000 dashboard.py' 2>&1 > streamlit.log &
-    echo $! > "$STREAMLIT_PID_FILE"
-fi
+
+    echo "Starting new process"
+    nohup sh -c "$start_command" > "$log_file" 2>&1 &
+    echo $! > "$pid_file"
+    echo "New process started with PID $(cat $pid_file)"
+}
+
+# Run Inference API
+handle_pid_file "api_pid.txt" "poetry run python data_ml_assignment/api/main.py" "api.log"
+
+# Run Streamlit Dashboard
+handle_pid_file "streamlit_pid.txt" "poetry run streamlit run --server.port 8000 dashboard.py" "streamlit.log"
